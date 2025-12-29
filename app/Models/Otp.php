bf39b0532f345808e8a3c56895b6a9b53fbbb3ed
Sayed Lashen <sayedlashen.dev@gmail.com>
2025-12-29 16:35:38 +0200

    chore: implement auto-pruning for expired OTP codes

--- app/Models/Otp.php
+++ app/Models/Otp.php
@@ -2,13 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\MassPrunable;
 use Illuminate\Database\Eloquent\Model;
 
 class Otp extends Model
 {
+    use MassPrunable;
     protected $fillable = [
         'email',
         'code',
         'expires_at',
     ];
+
+    public function prunable()
+    {
+        return static::where('expires_at', '<', now());
+    }
 }
